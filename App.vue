eb0a51439ef1e06c942152c348233b8f588520a8
GCZ-Zero <1203970284@qq.com>
2022-10-18 13:06:24 +0000

    6

--- App.vue
+++ App.vue
@@ -1,32 +1,13 @@
 <script>
-import { WButton, WTips, WTitle } from "@/WatchUI";
-import "@/WatchUI/index.css"
+import Buttons from "@/Examples/Buttons.vue"
 export default {
-  components: { WButton, WTips, WTitle },  
+  components: { Buttons }
 };
 </script>
 
 <template>
   <div class="app">
-    <WTitle color>演示站</WTitle>
-    <WTips>按钮</WTips>
-    <WButton>Default</WButton>
-    <WButton type="primary">
-      Primary
-    </WButton>
-    <WButton type="danger">
-      Danger
-    </WButton>
-    <WButton type="secondary">
-      Secondary
-    </WButton>
-    <WTips>圆角按钮</WTips>
-    <WButton radius>Rounds</WButton>
-    <WTips>双排小按钮</WTips>
-    <div class="flex">
-      <WButton small="left">Small</WButton>
-      <WButton small="right">Small</WButton>
-    </div>
+    <Buttons />
   </div>
 </template>
 
